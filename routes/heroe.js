15167d44f977f6cfef88201006d1e9502ccae76f
sebas <jesus.huamanculi1@unmsm.edu.pe>
2024-01-25 17:41:40 -0500

    commit 2

--- routes/heroe.js
+++ routes/heroe.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const {HeroeController} = require('../controllers/heroe');
-console.log(HeroeController);
+
 router.get('/',HeroeController.getHeroes);
 router.get('/heroe',HeroeController.getHeroByFilter);
 router.get('/lista',HeroeController.getHeroNamesList);
